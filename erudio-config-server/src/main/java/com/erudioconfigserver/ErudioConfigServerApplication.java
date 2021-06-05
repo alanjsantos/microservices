7fff4722870692b09c692dfb0ac03379e7f9d5f7
alan santos <alanjosedossantos011@gmail.com>
2021-06-05 11:08:42 -0300

    created project EurdioConfigServer

---
+++ erudio-config-server/src/main/java/com/erudioconfigserver/ErudioConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.erudioconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErudioConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErudioConfigServerApplication.class, args);
+	}
+
+}
---
+++ erudio-config-server/src/test/java/com/erudioconfigserver/ErudioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erudioconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErudioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
